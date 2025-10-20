642715b81c8e4f4c4a929658727911345b019246
syedmuhammadzaid <muhammad.zaid@code-avenue.com>
2025-10-21 02:05:43 +0500

    variables added

--- Portfolio/admin-panel/Utils/axiosInstance.ts
+++ Portfolio/admin-panel/Utils/axiosInstance.ts
@@ -3,7 +3,10 @@ import axios, { AxiosError, AxiosInstance } from "axios";
 
 // Create axios instance
 const axiosInstance: AxiosInstance = axios.create({
-    baseURL: process.env.NEXT_API_BASE_URL || "http://localhost:3000/api",
+    baseURL:
+        process.env.NEXT_PUBLIC_API_BASE_URL ||
+        process.env.NEXT_API_BASE_URL ||
+        "http://localhost:3000/api",
     timeout: 10000, // 10 seconds
     headers: {
         "Content-Type": "application/json",
